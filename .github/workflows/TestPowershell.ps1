c64f85961d29eb5f3625caf86c9e89f839118d85
Ramya Sri Addla <ramyasri.addla@blueyonder.com>
2022-06-05 23:32:04 +0530

    Testing

--- .github/workflows/TestPowershell.ps1
+++ .github/workflows/TestPowershell.ps1
@@ -1,5 +1,5 @@
-$key= '+0tLw4rPCHT5FuGnVE3rSWEkVHnVj/Zw4Itt0QPF9gfY34uipylDH1bCoUEcSGVJgNC3v/kBW2bTSRU5hZ9WnA=='
-
-$blobUrL= 'https://csg100320017998846f.blob.core.windows.net/fileupload/ImageServerFile.config'
-
-./AzCopy /Source:$blobUrL /Dest:C:\DownloadedFile /SourceKey:$key /Pattern:ImageServerFile.config
+$FileShareUrl = 'https://csg100320017998846f.blob.core.windows.net/fileupload/TestingFile.config'
+$Sas = $env:sas
+$OutputPath = 'C:\DownloadedFile\TestingFile.config'
+$FullUri = "$FileShareUrl$Sas"
+(New-Object System.Net.WebClient).DownloadFile($FullUri, $OutputPath)
