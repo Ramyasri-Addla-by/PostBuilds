0ddcec04f521aa23d07a65954611f55424a2ec1a
Ramya Sri Addla <ramyasri.addla@blueyonder.com>
2022-06-08 17:44:26 +0530

    invoke

--- .github/workflows/Powershell.ps1
+++ .github/workflows/Powershell.ps1
@@ -1,15 +1,15 @@
-$StorageAccName= "csg100320017998846f"
-$FileShareName="fileupload"
-$DownloadFileName="ImageServerFile.config"
+$StorageAccName= "str13"
+$FileShareName="imageserver"
+$DownloadFileName="IntactixImageServer.config"
 
 $Url = "https://"+$StorageAccName+.blob.core.windows.net/"+$FileShareName+"/"+$DownloadFileName"
 
 #Sas Token from storage account
-$Sas = '?sp=racw&st=2022-06-04T19:51:59Z&se=2022-06-07T03:51:59Z&spr=https&sv=2020-08-04&sr=c&sig=7UCKY1BedlFYGQESGBJp1XeWodZe1snBGKbEHAxLjHU%3D'
+$Sas = '?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-06-08T20:11:48Z&st=2022-06-08T12:11:48Z&spr=https&sig=DPsGv6kT%2Br8g1hWFpcL2QTJs1V5WlPU4mpdGRWUNx2c%3D'
 
 #path where the file is downloading
 #$OutputPath = 'C:\DownloadedFile\ImageServerFile.config'
-$OutputPath = 'C:\Intactix\ImageServer\IntactixImage.config'
+$OutputPath = 'C:\Tools\ImageServer\IntactixImageServer.config'
 $FullUrl = "$Url$Sas"
 (New-Object System.Net.WebClient).DownloadFile($FullUrl, $OutputPath)
 
