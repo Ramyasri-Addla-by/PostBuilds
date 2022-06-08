b9c5373c0aec56c1f68a3cb7cd17fefac5557a9c
Ramya Sri Addla <ramyasri.addla@blueyonder.com>
2022-06-08 18:03:10 +0530

    invoke

--- workloads/ImageserverPwrscrpt/Powershell.ps1
+++ workloads/ImageserverPwrscrpt/Powershell.ps1
@@ -2,7 +2,7 @@ $StorageAccName= "str13"
 $FileShareName="imageserver"
 $DownloadFileName="IntactixImageServer.config"
 
-$Url = "https://"+$StorageAccName+.blob.core.windows.net/"+$FileShareName+"/"+$DownloadFileName"
+$Url = "https://"+$StorageAccName+".blob.core.windows.net/"+$FileShareName+"/"+$DownloadFileName
 
 #Sas Token from storage account
 $Sas = '?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2022-06-08T20:11:48Z&st=2022-06-08T12:11:48Z&spr=https&sig=DPsGv6kT%2Br8g1hWFpcL2QTJs1V5WlPU4mpdGRWUNx2c%3D'
